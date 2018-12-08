dc230609782a050e9a5232b7c7662a4bf1995a63
ycf <921244819@qq.com>
2018-12-08 13:16:41 +0800

    菜单

--- src/main/java/com/hjljy/blog/controller/system/AccountController.java
+++ src/main/java/com/hjljy/blog/controller/system/AccountController.java
@@ -1,9 +1,12 @@
 package com.hjljy.blog.controller.system;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @Auther: HJLJY
  * @Date: 2018/12/6 0006 17:04
  * @Description:
  */
+@Controller
 public class AccountController {
 }
